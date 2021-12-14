d2d81aa81dbf6d5f651559bce32438eca5014c01
Patrick Martin <patrick.martin.r@gmail.com>
2021-12-14 20:53:49 +0000

    Fixes for shader include file.

--- include/shader.js
+++ include/shader.js
@@ -1,10 +1,11 @@
 /**
  * @typedef {!{
  *              load:function(!string,!string,number),
- *              updateOption:function(!string,(number|!Array<number>)):boolean,
- *              addOption:function(!string,(number|!Array<number>),!string):boolean,
+ *              updateOption:function(!string,(number|!Array<number>|!TypedArray)):boolean,
+ *              addOption:function(!string,(number|!Array<number>|!TypedArray),!string):boolean,
  *              bindShader:function((!WebGLRenderingContext|!WebGL2RenderingContext)),
  *              getShader:function():WebGLProgram,
+ *              getAttribute:function((!WebGLRenderingContext|!WebGL2RenderingContext),string):number,
  *              compile:function((!WebGLRenderingContext|!WebGL2RenderingContext),Object=,function(),string=),
  *          }}
  */
