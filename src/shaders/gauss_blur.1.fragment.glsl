d490b52b78822d51a749e70e765cce84c9196dbc
Patrick Martin <patrick.martin.r@gmail.com>
2022-01-01 19:48:10 +0000

    Some fixes to gaussian blur shaders. This may need to be optimized further.

--- src/shaders/gauss_blur.1.fragment.glsl
+++ src/shaders/gauss_blur.1.fragment.glsl
@@ -7,22 +7,19 @@ uniform float u_resolution_x;
 
 float normpdf(float n,float sigma)
 {
-	return 0.39894*exp(-0.5*n*n/(sigma*sigma))/sigma;
+    return 0.39894*exp(-0.5*n*n/(sigma*sigma))/sigma;
 }
 
 void main(){
     float pixel_x = 1.0/u_resolution_x;
-    
-    int mSize = int(((u_sigma/2.0)*(u_sigma/2.0)));
-    float kSize = float((mSize-1)/2);
-
     vec4 accumulator = texture2D(u_texture,v_texcoord)*normpdf(0.0,u_sigma);
     for(float i = 1.0; i < 512.0; i++){
-        if(i > kSize){
+        float gaussian_value = normpdf(i,u_sigma);
+        if(gaussian_value == 0.0){
             break;
         }
-        accumulator += texture2D(u_texture,clamp(v_texcoord+vec2(pixel_x*i,0),0.0,1.0)) * normpdf(i,u_sigma);
-        accumulator += texture2D(u_texture,clamp(v_texcoord-vec2(pixel_x*i,0),0.0,1.0)) * normpdf(i,u_sigma);
+        accumulator += texture2D(u_texture,clamp(v_texcoord+vec2(pixel_x*i,0),0.0,1.0)) * gaussian_value;
+        accumulator += texture2D(u_texture,clamp(v_texcoord-vec2(pixel_x*i,0),0.0,1.0)) * gaussian_value;
     }
     gl_FragColor = accumulator.rgba;
 }
--- src/shaders/gauss_blur.2.fragment.glsl
+++ src/shaders/gauss_blur.2.fragment.glsl
@@ -11,17 +11,15 @@ float normpdf(float n,float sigma)
 
 void main(){
     float pixel_y = 1.0/u_resolution_y;
-    
-    int mSize = int(((u_sigma/2.0)*(u_sigma/2.0)));
-    float kSize = float((mSize-1)/2);
-
     vec4 accumulator = texture2D(u_texture,v_texcoord)*normpdf(0.0,u_sigma);
     for(float i = 1.0; i < 512.0; i++){
-        if(i > kSize){
+        float gaussian_value = normpdf(i,u_sigma);
+        if(gaussian_value == 0.0){
             break;
         }
-        accumulator += texture2D(u_texture,clamp(v_texcoord+vec2(0,pixel_y*i),0.0,1.0)) * normpdf(i,u_sigma);
-        accumulator += texture2D(u_texture,clamp(v_texcoord-vec2(0,pixel_y*i),0.0,1.0)) * normpdf(i,u_sigma);
+        accumulator += texture2D(u_texture,clamp(v_texcoord+vec2(0,pixel_y*i),0.0,1.0)) * gaussian_value;
+        accumulator += texture2D(u_texture,clamp(v_texcoord-vec2(0,pixel_y*i),0.0,1.0)) * gaussian_value;
     }
+    
     gl_FragColor = accumulator.rgba;
 }
