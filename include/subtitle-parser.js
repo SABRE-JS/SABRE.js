4fa7b912a848485a433df9335ab664e4b4e3e605
Patrick Martin <patrick.martin.r@gmail.com>
2022-02-17 20:15:58 +0000

    More preparation for a serialization api.

--- include/subtitle-parser.js
+++ include/subtitle-parser.js
@@ -2,3 +2,15 @@
  * @typedef {!{info:Object,parser:Object,renderer:{events:Array<SSASubtitleEvent>}}}
  */
 var RendererData;
+
+/**
+ * @typedef {{
+ *      load:function(string,function(RendererData):void):void
+ * }}
+ */
+var SSAParser;
+
+/**
+ * @type {function(new:SSAParser)}
+ */
+sabre.Parser = function () {};
