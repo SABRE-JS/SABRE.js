e90fbe1008b9ee564586b6b0eed3139447369088
tcnevin <thomas.c.nevin@gmail.com>
2023-07-23 23:03:15 +0000

    refactor override tags into seperate file

---
+++ include/subtitle-tags.js
@@ -0,0 +1,29 @@
+/**
+ * @private
+ * @typedef {!{
+ *  ass_only:boolean,
+ *  ignore_exterior:boolean,
+ *  regular_expression:RegExp,
+ *  tag_handler:function(
+ *      {
+ *          start:number,
+ *          end:number
+ *      },
+ *      function(string):SSAStyleDefinition,
+ *      function(SSAStyleDefinition):void,
+ *      SSAStyleOverride,
+ *      SSALineStyleOverride,
+ *      function(SSALineTransitionTargetOverride):void,
+ *      Array<?string>,
+ *      boolean,
+ *      SSATransitionTargetOverride,
+ *      SSALineTransitionTargetOverride
+ *  )
+ * }}
+ */
+var OverrideTag;
+
+/**
+ * @type {function():Array<OverrideTag>}
+ */
+sabre.getOverrideTags = function () {return null};
